b6a55988a322b16cd08fe2efb52e5cc67eaa0871
gitjiho <jiho99322@gmail.com>
2025-06-02 22:18:20 +0900

    init: start project

---
+++ src/main/java/com/knu/ddip/DdipApplication.java
@@ -0,0 +1,13 @@
+package com.knu.ddip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knu/ddip/DdipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knu.ddip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
