224d40987d8fe605e4cd5a414361ac84cc7e8ebd
gitjiho <jiho99322@gmail.com>
2025-08-02 02:50:35 +0900

    Test: Redis의 테스트 컨테이너를 primary로 테스트 환경에서 설정

---
+++ src/test/java/com/knu/ddip/config/RedisTestConfig.java
@@ -0,0 +1,42 @@
+package com.knu.ddip.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@TestConfiguration
+public class RedisTestConfig {
+
+    @Bean
+    @Primary
+    public LettuceConnectionFactory testRedisConnectionFactory() {
+        String host = System.getProperty("spring.data.redis.host", "localhost");
+        int port = Integer.parseInt(System.getProperty("spring.data.redis.port", "6379"));
+        String password = System.getProperty("spring.data.redis.password", "");
+
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+        if (!password.isEmpty()) {
+            config.setPassword(password);
+        }
+
+        return new LettuceConnectionFactory(config);
+    }
+
+    @Bean
+    @Primary
+    public RedisTemplate<String, Object> testRedisTemplate(LettuceConnectionFactory testRedisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(testRedisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.afterPropertiesSet();
+        return template;
+    }
+}
