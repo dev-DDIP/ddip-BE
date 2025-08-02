39719ac3426eeb027c61b9f932f19cbc4fb2fd14
gitjiho <jiho99322@gmail.com>
2025-08-02 15:55:53 +0900

    Fix: UserController UserApi의 구현체로 수정

--- src/main/java/com/knu/ddip/user/presentation/controller/UserController.java
+++ src/main/java/com/knu/ddip/user/presentation/controller/UserController.java
@@ -4,31 +4,29 @@ import com.knu.ddip.auth.business.dto.JwtResponse;
 import com.knu.ddip.user.business.dto.SignupRequest;
 import com.knu.ddip.user.business.dto.UniqueMailResponse;
 import com.knu.ddip.user.business.service.UserService;
-import io.swagger.v3.oas.annotations.Operation;
+import com.knu.ddip.user.presentation.api.UserApi;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class UserController {
+public class UserController implements UserApi {
 
     private final UserService userService;
 
-    @PostMapping("/signup")
-    @Operation(summary = "회원 가입",
-            description = "Oauth 로직 통과 후 매칭되는 유저가 없을 시 회원가입을 진행한다.")
+    @Override
     public ResponseEntity<JwtResponse> signup(
             @Valid @RequestBody SignupRequest request) {
         JwtResponse jwtResponse = userService.signUp(request);
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(jwtResponse);
     }
 
-    @GetMapping("/my/mail")
-    @Operation(summary = "메일 중복조회",
-            description = "메일이 사용 가능한지 조회한다. 휴면유저/탈퇴한 유저의 메일도 사용 불가.")
+    @Override
     public ResponseEntity<UniqueMailResponse> checkEmailUniqueness(
             @RequestParam("v") String email) {
         UniqueMailResponse result = userService.checkEmailUniqueness(email);
