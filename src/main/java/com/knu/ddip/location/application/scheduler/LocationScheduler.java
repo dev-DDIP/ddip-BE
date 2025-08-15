ac12e104f813136784e1575d1adb4ab4e0edf363
Giheon Do <dgh0001@naver.com>
2025-08-15 14:44:42 +0900

    refactor: 스케줄러 응용계층과 인프라로 분리

--- src/main/java/com/knu/ddip/location/application/scheduler/LocationScheduler.java
+++ src/main/java/com/knu/ddip/location/application/scheduler/LocationScheduler.java
@@ -1,59 +1,20 @@
 package com.knu.ddip.location.application.scheduler;
 
+import com.knu.ddip.location.infrastructure.repositoroy.redis.RedisLocationExpiryCleanup;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.connection.RedisConnection;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.Cursor;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ScanOptions;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.nio.charset.StandardCharsets;
-import java.util.Set;
 
 @Component
 @RequiredArgsConstructor
 public class LocationScheduler {
 
-    private final RedisTemplate redisTemplate;
+    private final RedisLocationExpiryCleanup cleanup;
 
     @Scheduled(cron = "0 0 * * * *") // 매 정시
     public void cleanupExpiredUserLocations() {
         long now = System.currentTimeMillis();
-        ScanOptions scan = ScanOptions.scanOptions()
-                .match("cell:*:expiry")
-                .build();
-
-        RedisConnectionFactory connectionFactory = redisTemplate.getConnectionFactory();
-
-        if (connectionFactory == null) return;
-
-        try (
-                RedisConnection conn = connectionFactory.getConnection();
-                Cursor<byte[]> cursor = conn.keyCommands().scan(scan)
-        ) {
-            while (cursor.hasNext()) {
-                // 1. key 생성
-                byte[] expiryKey = cursor.next();
-                String expiryKeyStr = new String(expiryKey, StandardCharsets.UTF_8);
-                String usersKeyStr = expiryKeyStr.replace(":expiry", ":users");
-                byte[] usersKey = usersKeyStr.getBytes(StandardCharsets.UTF_8);
-
-                // 2. 만료된 멤버 수집 (-inf ~ now)
-                Set<byte[]> expired = conn.zSetCommands()
-                        .zRangeByScore(expiryKey, Double.NEGATIVE_INFINITY, (double) now);
-                if (expired == null || expired.isEmpty()) continue;
-
-                // 3. 파이프라인으로 SREM + ZREM
-                conn.openPipeline();
-                for (byte[] member : expired) {
-                    conn.sRem(usersKey, member);                 // SET에서 제거
-                    conn.zSetCommands().zRem(expiryKey, member); // ZSET에서도 제거
-                }
-                conn.closePipeline();
-            }
-        }
+        cleanup.cleanupExpiredUserLocations(now);
     }
 
 }
