841a81a3b0a59a80f9d887da52efd791ae51db99
Giheon Do <dgh0001@naver.com>
2025-08-18 14:47:51 +0900

    add: @RequireAuth 어노테이션 추가

--- src/main/java/com/knu/ddip/location/presentation/controller/LocationController.java
+++ src/main/java/com/knu/ddip/location/presentation/controller/LocationController.java
@@ -1,6 +1,7 @@
 package com.knu.ddip.location.presentation.controller;
 
 import com.knu.ddip.auth.domain.AuthUser;
+import com.knu.ddip.auth.presentation.annotation.RequireAuth;
 import com.knu.ddip.location.application.dto.UpdateMyLocationRequest;
 import com.knu.ddip.location.application.service.LocationService;
 import com.knu.ddip.location.presentation.api.LocationApi;
@@ -15,6 +16,7 @@ public class LocationController implements LocationApi {
     private final LocationService locationService;
 
     @Override
+    @RequireAuth
     public ResponseEntity<Void> updateMyLocation(AuthUser user, UpdateMyLocationRequest request) {
         locationService.saveUserLocationAtomic(user.getId(), request);
         return ResponseEntity.ok().build();
