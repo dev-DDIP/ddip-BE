384d34a82036199cb323e0ee3ac20c8375877cb6
gitjiho <jiho99322@gmail.com>
2025-08-26 22:35:58 +0900

    Chore: WebConfig에 인터셉터와 리졸버 오버라이딩

--- src/main/java/com/knu/ddip/common/config/WebConfig.java
+++ src/main/java/com/knu/ddip/common/config/WebConfig.java
@@ -1,15 +1,26 @@
 package com.knu.ddip.common.config;
 
+import com.knu.ddip.auth.presentation.interceptor.AuthInterceptor;
+import com.knu.ddip.auth.presentation.resolver.AuthUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authInterceptor;
+    private final AuthUserArgumentResolver authUserArgumentResolver;
 
     @Bean
     public RestTemplate restTemplate() {
@@ -28,5 +39,15 @@ public class WebConfig implements WebMvcConfigurer {
         config.setMaxAge(3600L);
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor).addPathPatterns("/**");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authUserArgumentResolver);
     }
 }
