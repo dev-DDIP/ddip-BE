60502d929a301e35bad1413f367be7d84e95a852
gitjiho <jiho99322@gmail.com>
2025-08-26 22:46:02 +0900

    Feat: Exception 정의 및 global handler 추가

---
+++ src/main/java/com/knu/ddip/ddipevent/exception/DdipBadRequestException.java
@@ -0,0 +1,7 @@
+package com.knu.ddip.ddipevent.exception;
+
+public class DdipBadRequestException extends RuntimeException {
+    public DdipBadRequestException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/knu/ddip/ddipevent/exception/DdipNotFoundException.java
@@ -0,0 +1,7 @@
+package com.knu.ddip.ddipevent.exception;
+
+public class DdipNotFoundException extends RuntimeException {
+    public DdipNotFoundException(String message) {
+        super(message);
+    }
+}
