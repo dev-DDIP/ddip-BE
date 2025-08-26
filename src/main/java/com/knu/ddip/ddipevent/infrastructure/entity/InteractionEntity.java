884a92e0c40a37cf8ebda772c93d8ad6da2fdb72
gitjiho <jiho99322@gmail.com>
2025-08-26 22:37:17 +0900

    Feat: InteractionEntity 정의

---
+++ src/main/java/com/knu/ddip/ddipevent/infrastructure/entity/InteractionEntity.java
@@ -0,0 +1,56 @@
+package com.knu.ddip.ddipevent.infrastructure.entity;
+
+import com.knu.ddip.ddipevent.domain.ActionType;
+import com.knu.ddip.ddipevent.domain.ActorRole;
+import com.knu.ddip.ddipevent.domain.MessageCode;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.type.SqlTypes;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "interaction")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class InteractionEntity {
+    @Id
+    @UuidGenerator
+    @Column(columnDefinition = "char(36)", updatable = false, nullable = false)
+    @JdbcTypeCode(SqlTypes.CHAR)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ddip_event_id")
+    private DdipEventEntity ddipEvent;
+
+    @Column(columnDefinition = "char(36)")
+    @JdbcTypeCode(SqlTypes.CHAR)
+    private UUID actorId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ActorRole actorRole;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ActionType actionType;
+
+    @Enumerated(EnumType.STRING)
+    private MessageCode messageCode;
+
+    @Column(columnDefinition = "char(36)", updatable = false)
+    @JdbcTypeCode(SqlTypes.CHAR)
+    private UUID relatedPhotoId;
+
+    @Column(nullable = false)
+    private Instant timestamp;
+}
