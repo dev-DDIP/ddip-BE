b5602885d2a95a37f2c3c57d0f42294b9b13643c
gitjiho <jiho99322@gmail.com>
2025-09-08 12:27:55 +0900

    Feat: DDIP API 기반 presentation단 controller 구현체 구현

--- src/main/java/com/knu/ddip/ddipevent/presentation/controller/DdipController.java
+++ src/main/java/com/knu/ddip/ddipevent/presentation/controller/DdipController.java
@@ -1,13 +1,12 @@
 package com.knu.ddip.ddipevent.presentation.controller;
 
 import com.knu.ddip.auth.domain.AuthUser;
-import com.knu.ddip.ddipevent.application.dto.CreateDdipRequestDto;
-import com.knu.ddip.ddipevent.application.dto.DdipEventDetailDto;
-import com.knu.ddip.ddipevent.application.dto.DdipEventSummaryDto;
-import com.knu.ddip.ddipevent.application.dto.FeedRequestDto;
+import com.knu.ddip.common.dto.StringTypeResponse;
+import com.knu.ddip.ddipevent.application.dto.*;
 import com.knu.ddip.ddipevent.application.service.DdipService;
 import com.knu.ddip.ddipevent.presentation.api.DdipApi;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,8 +32,44 @@ public class DdipController implements DdipApi {
     }
 
     @Override
-    public ResponseEntity<DdipEventDetailDto> getDdipEventDetail(UUID ddipId) {
-        DdipEventDetailDto ddipDetail = ddipService.getDdipEventDetail(ddipId);
+    public ResponseEntity<DdipEventDetailDto> getDdipEventDetail(UUID eventId) {
+        DdipEventDetailDto ddipDetail = ddipService.getDdipEventDetail(eventId);
         return ResponseEntity.ok(ddipDetail);
+    }
+
+    @Override
+    public ResponseEntity<StringTypeResponse> applyDdipEvent(UUID eventId, AuthUser authUser) {
+        ddipService.applyDdipEvent(eventId, authUser.getId());
+        return ResponseEntity.status(HttpStatus.OK).body(new StringTypeResponse("정상적으로 지원되었습니다."));
+    }
+
+    @Override
+    public ResponseEntity<StringTypeResponse> selectApplicantForDdipEvent(UUID eventId, SelectApplicantRequest selectApplicantRequest, AuthUser authUser) {
+        ddipService.selectApplicantForDdipEvent(eventId, selectApplicantRequest, authUser.getId());
+        return ResponseEntity.status(HttpStatus.OK).body(new StringTypeResponse("정상적으로 수행자를 선택하였습니다."));
+    }
+
+    @Override
+    public ResponseEntity<DdipEventDetailDto> uploadPhotoForDdipEvent(UUID eventId, PhotoUploadRequest photoUploadRequest, AuthUser authUser) {
+        DdipEventDetailDto ddipEventDetailDto = ddipService.uploadPhotoForDdipEvent(eventId, photoUploadRequest, authUser.getId());
+        return ResponseEntity.ok(ddipEventDetailDto);
+    }
+
+    @Override
+    public ResponseEntity<DdipEventDetailDto> updatePhotoFeedback(UUID eventId, UUID photoId, PhotoFeedbackRequest photoFeedbackRequest, AuthUser authUser) {
+        DdipEventDetailDto ddipEventDetailDto = ddipService.updatePhotoFeedback(eventId, photoId, photoFeedbackRequest, authUser.getId());
+        return ResponseEntity.ok(ddipEventDetailDto);
+    }
+
+    @Override
+    public ResponseEntity<DdipEventDetailDto> completeDdipEventMission(UUID eventId, AuthUser authUser) {
+        DdipEventDetailDto ddipEventDetailDto = ddipService.completeDdipEventMission(eventId, authUser.getId());
+        return ResponseEntity.ok(ddipEventDetailDto);
+    }
+
+    @Override
+    public ResponseEntity<DdipEventDetailDto> cancelDdipEventMission(UUID eventId, AuthUser authUser) {
+        DdipEventDetailDto ddipEventDetailDto = ddipService.cancelDdipEventMission(eventId, authUser.getId());
+        return ResponseEntity.ok(ddipEventDetailDto);
     }
 }
