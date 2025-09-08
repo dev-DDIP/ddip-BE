4248c237dd2a1c839924fcd9ab79dab483408292
gitjiho <jiho99322@gmail.com>
2025-09-08 12:24:19 +0900

    Feat: DDIP Event 비즈니스 로직 루트 애그리거트에 구현

--- src/main/java/com/knu/ddip/ddipevent/domain/DdipEvent.java
+++ src/main/java/com/knu/ddip/ddipevent/domain/DdipEvent.java
@@ -2,6 +2,7 @@ package com.knu.ddip.ddipevent.domain;
 
 import com.knu.ddip.ddipevent.exception.DdipBadRequestException;
 import com.knu.ddip.ddipevent.exception.DdipForbiddenException;
+import com.knu.ddip.ddipevent.exception.DdipNotFoundException;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -43,7 +44,7 @@ public class DdipEvent {
                 .build();
     }
 
-    public void apply(UUID applicantId) {
+    public DdipEvent apply(UUID applicantId) {
         if (this.status != DdipStatus.OPEN) {
             throw new DdipBadRequestException("이미 마감된 띱입니다.");
         }
@@ -56,11 +57,13 @@ public class DdipEvent {
                     .actorId(applicantId)
                     .actorRole(ActorRole.RESPONDER)
                     .actionType(ActionType.APPLY)
+                    .timestamp(Instant.now())
                     .build());
-        }
+            return this;
+        } else throw new DdipBadRequestException("이미 지원한 띱입니다.");
     }
 
-    public void selectResponder(UUID requesterId, UUID responderId) {
+    public DdipEvent selectResponder(UUID requesterId, UUID responderId) {
         if (!Objects.equals(this.requesterId, requesterId)) {
             throw new DdipForbiddenException("띱을 등록한 사용자만 수행자를 선택할 수 있습니다.");
         }
@@ -76,6 +79,133 @@ public class DdipEvent {
                 .actorId(requesterId)
                 .actorRole(ActorRole.REQUESTER)
                 .actionType(ActionType.SELECT_RESPONDER)
+                .timestamp(Instant.now())
                 .build());
+        return this;
+    }
+
+    public DdipEvent uploadPhoto(UUID responderId, String photoUrl, Double latitude, Double longitude, String responderComment) {
+        if (!this.selectedResponderId.equals(responderId)) {
+            throw new DdipForbiddenException("띱의 선택된 수행자만 사진을 업로드할 수 있습니다.");
+        }
+        if (this.status != DdipStatus.IN_PROGRESS) {
+            throw new DdipBadRequestException("진행중인 띱에만 사진을 업로드할 수 있습니다.");
+        }
+        if (photoUrl == null || photoUrl.isBlank()) {
+            throw new DdipBadRequestException("photoUrl 값이 없습니다.");
+        }
+        this.photos.add(Photo.builder()
+                .photoUrl(photoUrl)
+                .latitude(latitude)
+                .longitude(longitude)
+                .timestamp(Instant.now())
+                .status(PhotoStatus.PENDING)
+                .responderComment(responderComment)
+                .build());
+        this.interactions.add(Interaction.builder()
+                .actorId(responderId)
+                .actorRole(ActorRole.RESPONDER)
+                .actionType(ActionType.SUBMIT_PHOTO)
+                .timestamp(Instant.now())
+                .build());
+        return this;
+    }
+
+    public DdipEvent updatePhotoFeedback(UUID requesterOrResponderId, UUID photoId, PhotoStatus status, String feedback) {
+        if (status.equals(PhotoStatus.PENDING)) {
+            throw new DdipForbiddenException("Pending 상태로 변경할수는 없습니다.");
+        }
+        Photo targetPhoto = findPhotoOrThrow(photoId);
+        if (Objects.equals(requesterOrResponderId, this.requesterId)) { // 주체가 요청자
+            if (status.equals(PhotoStatus.APPROVED)) {
+                targetPhoto.approve();
+            } else {
+                boolean isQuestion = targetPhoto.feedbackByRequester(feedback);
+                if (isQuestion) {
+                    this.interactions.add(Interaction.builder()
+                            .actorId(this.requesterId)
+                            .actorRole(ActorRole.REQUESTER)
+                            .actionType(ActionType.ASK_QUESTION)
+                            .comment(feedback)
+                            .relatedPhotoId(photoId)
+                            .timestamp(Instant.now())
+                            .build());
+                } else {
+                    this.interactions.add(Interaction.builder()
+                            .actorId(this.requesterId)
+                            .actorRole(ActorRole.REQUESTER)
+                            .actionType(ActionType.REQUEST_REVISION)
+                            .comment(feedback)
+                            .relatedPhotoId(photoId)
+                            .timestamp(Instant.now())
+                            .build());
+                }
+            }
+            return this;
+        } else if (Objects.equals(requesterOrResponderId, this.selectedResponderId)) { // 주체가 수행자
+            targetPhoto.feedbackByResponder(feedback);
+            this.interactions.add(Interaction.builder()
+                    .actorId(this.selectedResponderId)
+                    .actorRole(ActorRole.RESPONDER)
+                    .actionType(ActionType.ANSWER_QUESTION)
+                    .comment(feedback)
+                    .relatedPhotoId(photoId)
+                    .timestamp(Instant.now())
+                    .build());
+            return this;
+        } else
+            throw new DdipBadRequestException("띱의 수행자 또는 요청자만 사진에 피드백을 남길 수 있습니다.");
+    }
+
+    public DdipEvent complete(UUID requesterId) {
+        if (!Objects.equals(this.requesterId, requesterId)) {
+            throw new DdipForbiddenException("띱을 등록한 사용자만 완료할 수 있습니다.");
+        }
+        if (photos.isEmpty()) {
+            throw new DdipBadRequestException("업로드된 사진이 없습니다.");
+        }
+        Photo lastPhoto = photos.get(photos.size() - 1);
+        if (lastPhoto.statusIsNotApproved()) {
+            throw new DdipForbiddenException("최종 사진의 Status가 Approved가 아닙니다.");
+        }
+        this.status = DdipStatus.COMPLETED;
+        this.interactions.add(Interaction.builder()
+                .actorId(requesterId)
+                .actorRole(ActorRole.RESPONDER)
+                .actionType(ActionType.APPROVE)
+                .timestamp(Instant.now())
+                .build());
+        return this;
+    }
+
+    public DdipEvent cancel(UUID requesterOrResponderId) {
+        if (Objects.equals(requesterOrResponderId, this.requesterId)) { // 주체가 요청자
+            this.status = DdipStatus.CANCELED;
+            this.interactions.add(Interaction.builder()
+                    .actorId(this.requesterId)
+                    .actorRole(ActorRole.REQUESTER)
+                    .actionType(ActionType.CANCEL_BY_REQUESTER)
+                    .timestamp(Instant.now())
+                    .build());
+            return this;
+        } else if (Objects.equals(requesterOrResponderId, this.selectedResponderId)) { // 주체가 수행자
+            this.status = DdipStatus.CANCELED;
+            this.interactions.add(Interaction.builder()
+                    .actorId(this.selectedResponderId)
+                    .actorRole(ActorRole.RESPONDER)
+                    .actionType(ActionType.GIVE_UP_BY_RESPONDER)
+                    .timestamp(Instant.now())
+                    .build());
+            return this;
+        } else
+            throw new DdipBadRequestException("띱의 수행자 또는 요청자만 취소할 수 있습니다.");
+    }
+
+    private Photo findPhotoOrThrow(UUID photoId) {
+        return this.photos.stream()
+                .filter(photo -> photo.getPhotoId().equals(photoId))
+                .findFirst()
+                .orElseThrow(() ->
+                        new DdipNotFoundException("해당 ID를 가진 Photo가 존재하지 않습니다"));
     }
 }
